6dcceaab058a4073296aaca77cbfd157c8b5877c
JYSSoftWare <JYSSoftware@gmail.com>
2015-10-06 13:06:55 -0700

    we

---
+++ BasicType.java
@@ -0,0 +1,63 @@
+/**
+ * Created by jinyongsuk on 10/2/15.
+ */
+abstract class BasicType extends Type {
+
+    // Name of the Type (e.g., int, bool, some structdef, etc.)
+    private String m_typeName;
+    private int m_size;
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    public BasicType(String strName, int size)
+    {
+        super(strName, size);
+        setName(strName);
+        setSize(size);
+    }
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    public String getName()
+    {
+        return m_typeName;
+    }
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    private void setName(String str)
+    {
+        m_typeName = str;
+    }
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    public int getSize()
+    {
+        return m_size;
+    }
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    private void setSize(int size)
+    {
+        m_size = size;
+    }
+
+    //----------------------------------------------------------------
+    //	It will be helpful to ask a Type what specific Type it is.
+    //	The Java operator instanceof will do this, but you may
+    //	also want to implement methods like isNumeric(), isInt(),
+    //	etc. Below is an example of isInt(). Feel free to
+    //	change this around.
+    //----------------------------------------------------------------
+    public boolean  isError()   { return false; }
+    public boolean  isInt()	    { return false; }
+
+
+}
