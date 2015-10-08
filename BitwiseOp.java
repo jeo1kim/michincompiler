8f368d7b872b79476682b0eda1c0a209199cb568
yongsukjin <JYSSoftware@gmail.com>
2015-10-08 13:41:04 -0700

    Operator and Type Hierachy

---
+++ BitwiseOp.java
@@ -0,0 +1,33 @@
+/**
+ * Created by jinyongsuk on 10/8/15.
+ */
+abstract class BitwiseOp extends BinaryOp {
+    // Name of the Type (e.g., int, bool, some structdef, etc.)
+    private String m_OpName;
+    //private int m_size;
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    public BitwiseOp(String strName )
+    {
+        super(strName);
+        //setSize(size);
+    }
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    public String getName()
+    {
+        return m_OpName;
+    }
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    private void setName(String str)
+    {
+        m_OpName = str;
+    }
+}
---
+++ DivideOp.java
@@ -0,0 +1,33 @@
+/**
+ * Created by jinyongsuk on 10/8/15.
+ */
+public class DivideOp extends ArithmeticOp {
+    // Name of the Type (e.g., int, bool, some structdef, etc.)
+    private String m_OpName;
+    //private int m_size;
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    public DivideOp(String strName )
+    {
+        super(strName);
+        //setSize(size);
+    }
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    public String getName()
+    {
+        return m_OpName;
+    }
+
+    //----------------------------------------------------------------
+    //
+    //----------------------------------------------------------------
+    private void setName(String str)
+    {
+        m_OpName = str;
+    }
+}
