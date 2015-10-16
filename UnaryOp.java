8c41138dd73bb881e1e370c29865f11e4e8750b1
Eui Won Kim <ewk001@ucsd.edu>
2015-10-15 19:09:47 -0700

    incOp fixed

--- UnaryOp.java
+++ UnaryOp.java
@@ -20,16 +20,17 @@ abstract class UnaryOp extends Operator {
     {
         //System.out.println(a.getType().isNumeric());
         Type aType = a.getType();
-
+        
+        if(!a.isModLValue() && !a.isError()){
+            //      "Operand to %O is not a modifiable L-value.";
+            return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Lval, opName));
+        }
         if( !(aType instanceof NumericType) && !a.isError())
         {
             // "Incompatible type %T to operator %O, equivalent to int, float, or pointer expected.";
             return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Type, aType.getName(),opName));
         }
-        if(!a.isModLValue() && !a.isError()){
-            //      "Operand to %O is not a modifiable L-value.";
-            return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Lval, opName));
-        }
+
         else
         {
             STO result = new ConstSTO( a.getName() , a.getType()); // do i make a new STO or return the old one.
