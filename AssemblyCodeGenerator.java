ac107301b8339575d7b6226132af6a3302977074
Dayoun Lee <dal052@ucsd.edu>
2015-10-31 13:30:33 -0700

    base AssemblyGenerator java file

---
+++ AssemblyCodeGenerator.java
@@ -0,0 +1,105 @@
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+
+public class AssemblyCodeGenerator {
+    // 1
+    private int indent_level = 0;
+
+    // 2
+    private static final String ERROR_IO_CLOSE =
+            "Unable to close fileWriter";
+    private static final String ERROR_IO_CONSTRUCT =
+            "Unable to construct FileWriter for file %s";
+    private static final String ERROR_IO_WRITE =
+            "Unable to write to fileWriter";
+
+    // 3
+    private FileWriter fileWriter;
+
+    // 4
+    private static final String FILE_HEADER =
+            "/*\n" +
+                    " * Generated %s\n" +
+                    " */\n\n";
+
+    // 5
+    private static final String SEPARATOR = "\t";
+
+    // 6
+    private static final String SET_OP = "set";
+    private static final String TWO_PARAM = "%s" + SEPARATOR + "%s, %s\n";
+
+    public AssemblyCodeGenerator(String fileToWrite) {
+        try {
+            fileWriter = new FileWriter(fileToWrite);
+
+            // 7
+            writeAssembly(FILE_HEADER, (new Date()).toString());
+        } catch (IOException e) {
+            System.err.printf(ERROR_IO_CONSTRUCT, fileToWrite);
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+    // 8
+    public void decreaseIndent() {
+        indent_level--;
+    }
+
+    public void dispose() {
+        try {
+            fileWriter.close();
+        } catch (IOException e) {
+            System.err.println(ERROR_IO_CLOSE);
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    public void increaseIndent() {
+        indent_level++;
+    }
+
+
+
+    // 9
+    public void writeAssembly(String template, String ... params) {
+        StringBuilder asStmt = new StringBuilder();
+
+        // 10
+        for (int i=0; i < indent_level; i++) {
+            asStmt.append(SEPARATOR);
+        }
+
+        // 11
+        asStmt.append(String.format(template, (Object[])params));
+
+        try {
+            fileWriter.write(asStmt.toString());
+        } catch (IOException e) {
+            System.err.println(ERROR_IO_WRITE);
+            e.printStackTrace();
+        }
+    }
+
+    // 12
+    public static void main(String args[]) {
+        AssemblyCodeGenerator myAsWriter = new AssemblyCodeGenerator("rc.s");
+
+        myAsWriter.increaseIndent();
+        myAsWriter.writeAssembly(TWO_PARAM, SET_OP, String.valueOf(4095), "%l0");
+        myAsWriter.increaseIndent();
+        myAsWriter.writeAssembly(TWO_PARAM, SET_OP, String.valueOf(1024), "%l1");
+        myAsWriter.decreaseIndent();
+
+        myAsWriter.writeAssembly(TWO_PARAM, SET_OP, String.valueOf(512), "%l2");
+
+        myAsWriter.decreaseIndent();
+        myAsWriter.dispose();
+    }
+}
