2a29b3120e5faa98def115841b5db4f195461a11
Dayoun Lee <dal052@ucsd.edu>
2015-11-04 14:53:31 -0800

    created writeFunctionDecl and writeLocalVar. works for functs w/o parameters and local vars that is initialized only. see comments (TODO) for further details

--- AssemblyCodeGenerator.java
+++ AssemblyCodeGenerator.java
@@ -30,6 +30,8 @@ public class AssemblyCodeGenerator {
     // 5
     private static final String SEPARATOR = "\t";
 
+    private int offset = 0; //to check what offset of that value is
+    private String infunc = ""; //used as a flag to check if it is inside function or not i belive there will be a better way tho 
     // 6
     //parameters
     private static final String ONE_PARAM = "%s" + SEPARATOR + "%s\n";
@@ -39,14 +41,15 @@ public class AssemblyCodeGenerator {
 
 
     public static final String OFFSET = " = -(92 + %d) & -8";
-    public static final String SP_REG = "%sp";
-    public static final String FP_REG = "%fp";
+    public static final String SP = "%sp";
+    public static final String FP = "%fp";
 
     //synthetic instructions
     private static final String SET_OP = "set";
     private static final String SAVE_OP = "save";
-    private static final String RET_OP = "ret";
-    private static final String RESTORE_OP = "restore";
+    private static final String RET_OP = "ret\n";
+    private static final String RESTORE_OP = "restore\n";
+    private static final String NOP_OP = "nop\n";
 
     private static final String CMP_OP = "cmp";
     private static final String JMP_OP = "jmp";
@@ -57,6 +60,12 @@ public class AssemblyCodeGenerator {
     private static final String INC_OP = "inc";
     private static final String DEC_OP = "dec";
     private static final String MOV_OP = "mov";
+
+    private static final String ST_OP = "st";
+    private static final String LD_OP = "ld";
+
+    private static final String ADD_OP = "add";
+
 
     //section
     private static final String SECTION = ".section \"%s\"\n";
@@ -71,8 +80,13 @@ public class AssemblyCodeGenerator {
     private static final String VARCOLON = "%s:"+SEPARATOR;
     //private static final String ASCIZ = ".asciz";
 
-    private static final String SAVE_FUNC = "SAVE.%s.%s";
+    private static final String SAVE_MAIN = "SAVE.main";
+    private static final String SAVE_FUNC = "SAVE.%s";
     private static final String FINI_FUNC = "%s.%s.fini";
+
+    private static final String NL = "\n";
+    private static final String O0 = "%o0";
+    private static final String O1 = "%o1";
 
 
 
@@ -89,15 +103,89 @@ public class AssemblyCodeGenerator {
         }
     }
 
-    public void writeVariable(STO sto, STO init){
+    //takes care of the offset inc and dec
+    public int getOffset(){
+        return this.offset;
+    }
+    public int increaseOffset(){
+        return this.offset += 4;
+    }
+    public int decreaseOffset(){
+        return this.offset -= 4;
+    }
 
+    //check if the variable is static or global
+    public void writeVariable(STO sto, STO init){
         if(sto.isStatic() || sto.isGlobal()){
             this.writeGlobalStaticVariable(sto, init);
+        }
+        else {
+            writeLocalVariable(sto, init);
         }
 
     }
 
+    //simple function decl
+    //TODO: does not take care of parameters yet
+    public void writeFunctionDecl_1(STO sto){
+        infunc = sto.getName();
+        String name = sto.getName();
+        increaseIndent();
+        writeAssembly(SECTION, TEXT_SEC);
+        writeAssembly(GLOBAL, name);
+        decreaseIndent();
+        writeAssembly(VARCOLON, name);
+        writeAssembly(NL);
+
+        if(name.equals("main")){
+            increaseIndent();
+            writeAssembly(TWO_PARAM, SET_OP, SAVE_MAIN, "%g1");
+            writeAssembly(THREE_PARAM, SAVE_OP, SP, "%g1", SP);
+            decreaseIndent();
+            writeAssembly(NL);
+        }
+    }
+
+    //TODO: take care of when init not there too
+    public void writeLocalVariable(STO sto, STO init){
+        String sectioncheck;
+        String val = ""; //later used for init if init is null to handle null pointer
+
+        increaseIndent();
+        if((init != null)){     //check if init is not null store the value
+            if(init.getType().isInt()) {
+                int constval = init.getIntValue();
+                val = iString(constval);
+            }
+            if(init.getType().isFloat()){
+                float constval = init.getFloatValue();
+                val = Float.toString(constval);
+            }
+            if(init.getType().isBool()){
+                if(init.getValue().equals(BigDecimal.ZERO)){
+                    val = "0";
+                }else {
+                    val = "1";
+                }
+            }
+            decreaseOffset();
+            //create space
+            writeAssembly(TWO_PARAM, SET_OP, iString(offset), O1);
+            writeAssembly(THREE_PARAM, ADD_OP, FP, O1, O1);
+            //set value
+            writeAssembly(TWO_PARAM, SET_OP, val, O0);
+            writeAssembly(TWO_PARAM, ST_OP, O1, "["+O1+"]");
+            decreaseIndent();
+            writeAssembly(NL);
+        }
+        else{
+            //here nothing done yet
+            sectioncheck = TEXT_SEC;
+        }
+    }
+    //writes assembly for variables that is glocal or static
     public void writeGlobalStaticVariable(STO sto, STO init){
+        increaseIndent();
         String sectioncheck;
         //Type sType = sto.getType();
 
@@ -126,37 +214,53 @@ public class AssemblyCodeGenerator {
                     val = "1";
                 }
             }
-            if(!init.isConst()){
+
+            //any global variable not initialized when declared is set to value 0
+            if(!init.isConst() && sto.isGlobal()){
                 size = 0;
                 val = iString(size);
             }
+
         }
 
-        //indent
-        increaseIndent();
+
         writeAssembly(SECTION, sectioncheck);
         writeAssembly(ALIGN, iString(stotype.getSize()));
 
         String name = sto.getName();
         if (!sto.isStatic()) { // global
             writeAssembly(GLOBAL, name);
-            decreaseIndent();
-            writeAssembly(VARCOLON, name);
-            writeAssembly(WORD, val);
-        } else {
-            decreaseIndent();
-            writeAssembly(VARCOLON, name);
-            if (init != null) {
-                writeAssembly(WORD, val);
-            } else {
-                writeAssembly(SKIP, val);
+
+        }
+        //take care of the name change if static is inside function
+        if(sto.isStatic()){
+            if(infunc != null){
+                name = infunc + "." + sto.getName();
+            }
+            //if sto is static and is not global get the size of the type and write as word even tho it is bss
+            if(!sto.isGlobal()){
+                val = iString(stotype.getSize());
             }
         }
+        decreaseIndent();
+        writeAssembly(VARCOLON, name);
+        if((sectioncheck == BSS_SEC) && sto.isGlobal()){
+            writeAssembly(SKIP, iString(size));
+        } else {
+            writeAssembly(WORD, val);
+        }
+
+        writeAssembly(NL);
     }
+
+
+
+
 
     public String iString(int val){
         return Integer.toString(val);
     }
+
 
     // 8
     public void decreaseIndent() {
