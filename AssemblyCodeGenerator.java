cafa621aa0d47872325341095d90a72cf2b948fb
Eui Won Kim <ewk001@ucsd.edu>
2015-11-08 02:33:11 -0800

    cout with float addition

--- AssemblyCodeGenerator.java
+++ AssemblyCodeGenerator.java
@@ -534,7 +534,7 @@ public class AssemblyCodeGenerator {
         //offset = newoffset;
         writeAssembly(TWO_PARAM, SET_OP, iString(getOffset()), L7);
         writeAssembly(THREE_PARAM, ADD_OP, FP, L7, L7);
-        writeAssembly(TWO_PARAM, ST_OP, O0, "[" +L7+ "]");
+        writeAssembly(TWO_PARAM, ST_OP, O1, "[" +L7+ "]");
         writeAssembly(TWO_PARAM, LD_OP, "[" + L7 + "]", F1);
         writeAssembly(TWO_PARAM, FITOS, F1, F1);
 
