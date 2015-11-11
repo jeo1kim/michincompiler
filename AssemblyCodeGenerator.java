348f5355c80bb61b5a06bf22a479d6d48cf6d147
Eui Won Kim <ewk001@ucsd.edu>
2015-11-11 10:24:01 -0800

    assignmetn

--- AssemblyCodeGenerator.java
+++ AssemblyCodeGenerator.java
@@ -867,7 +867,7 @@ public class AssemblyCodeGenerator {
         //sto.setSparcOffset(newoffset);
         //offset = newoffset;
         writeAssembly(TWO_PARAM, SET_OP, iString(getOffset()), L7);
-        writeAssembly(THREE_PARAM, ADD_OP, globalreg, L7, L7);
+        writeAssembly(THREE_PARAM, ADD_OP, FP, L7, L7);
         writeAssembly(TWO_PARAM, ST_OP, order, "[" + L7 + "]");
         //writeAssembly(TWO_PARAM, LD_OP, "[" + L7 + "]", register);
         writefloatreg(floatreg++);
